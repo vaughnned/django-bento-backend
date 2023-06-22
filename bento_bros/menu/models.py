3f7fb5791b53148acc5fa1c3d8916476c5385d37
Vaughn Nedderman <vaughn@nedderman.com>
2023-06-22 14:01:51 -0400

    initial commit

---
+++ bento_bros/menu/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Appetizer(models.Model):
+    name = models.CharField(max_length=50)
+    japanese_name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    description = models.TextField(max_length=150)
+
+    def __str__(self):
+        return self.name
+    
+class MainCourse(models.Model):
+    name = models.CharField(max_length=50)
+    japanese_name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    description = models.TextField(max_length=150)
+
+    def __str__(self):
+        return self.name
+    
+class Dessert(models.Model):
+    name = models.CharField(max_length=50)
+    japanese_name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    description = models.TextField(max_length=150)
+    
+    def __str__(self):
+        return self.name
